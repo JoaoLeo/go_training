016ac03141ce4d65a95c62a5c0f2d2d4a6d1d358
João Leonardo <joaoleonardo9921@gmail.com>
2025-11-01 09:54:02 -0300

    hello worlds

--- http-hello-world.go
+++ http-hello-world.go
@@ -10,7 +10,12 @@ func greet(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World! %s", time.Now())
 }
 
+func timeNow(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Agora são: %s", time.Now())
+}
+
 func main() {
 	http.HandleFunc("/", greet)
+	http.HandleFunc("/time", timeNow)
 	http.ListenAndServe(":8080", nil)
 }
